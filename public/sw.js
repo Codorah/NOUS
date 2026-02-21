24f74ae46cd356c3179a12794af1039acc4fc772
Tj-d-tech <mydidishopp@gmail.com>
2026-02-21 08:59:56 +0000

    Mise a jour UI mobile, branding NOUS, export journal lisible

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "journal-infinite-v2";
+﻿const CACHE_NAME = "journal-infinite-v3";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -74,4 +74,5 @@ self.addEventListener("notificationclick", (event) => {
     })
   );
 });
+
 
